8617bfbe443add354ae7b2221d32a0735c4de39f
Nkosi <njceles@gmail.com>
2025-05-20 19:06:03 +0200

    renamed the feature and steps def

--- src/test/java/Runners/CalculatorRunner.java
+++ src/test/java/Runners/CalculatorRunner.java
@@ -5,7 +5,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features = "src/test/resources/Features",
+        features = "src/test/resources/Features/Calculator.feature",
         glue = {"Steps"},
         plugin = {"pretty", "html:target/cucumber-reports.html", "json:target/cucumber.json",
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
